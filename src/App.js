9a3166d14052d20aecf1eb1784cb219f611aa2e4
nrnlimbu <nrntheguwa@gmail.com>
2021-01-24 13:22:07 +1100

    update menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
 
+const Shop=()=>(
+  <div>
+    <h1>This is Shop Page</h1>
+  </div>
+)
+
+const Detail=({match})=>{
+  console.log(match.params)
+  return <div>
+    <h1>This is Detail Page</h1>
+  </div>
+}
 function App() {
   return (
     <div >
-     <HomePage/>
+     <Switch>
+       <Route exact path='/' component={HomePage}/>
+       <Route exact path='/shop' component={Shop}/>
+       <Route exact path='/shop/:detailID' component={Detail}/>
+     </Switch>
     </div>
   );
 }
