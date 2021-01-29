faff1ff504131f9ccd50bd15ebdbc40508b152b5
nrnlimbu <nrntheguwa@gmail.com>
2021-01-29 16:54:15 +1100

    removing logger and contact page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,7 +17,6 @@ const Header=({currentUser, hidden, msg})=>{
             </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/contact'>CONTACT</Link>
             {   
                 currentUser ? <div className='div-option' onClick={()=>auth.signOut()}>SIGN OUT</div>
                 : <Link className='option' to='/signin'>SIGNIN</Link>
